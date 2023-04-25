d28cf1875ce9470bdb7b149328804131a4805d2e
Amon Peres <ext.amon.peres@farfetch.com>
2023-04-25 16:02:56 -0300

    Initial Commit

---
+++ Family Expenses Tracker/Family_Expenses_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_Expenses_TrackerApp.swift
+//  Family Expenses Tracker
+//
+//  Created by Amon Peres on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Family_Expenses_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
